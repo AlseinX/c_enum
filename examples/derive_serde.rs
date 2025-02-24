f52c4a3de41a9f18498dc272dbfc997df0cfa50c
yhan <yhan@asd.com>
2025-02-24 13:12:05 +0800

    feat: enhanced conversion to primitive types

--- examples/derive_serde.rs
+++ examples/derive_serde.rs
@@ -5,6 +5,7 @@ use thiserror::Error;
 #[ffi_enum]
 #[derive(Debug, Serialize, Deserialize, Error, Hash)]
 #[serde(rename_all = "snake_case")]
+#[repr(i16)]
 pub enum Animal {
     #[error("unknown animal")]
     Cat,
@@ -17,8 +18,10 @@ fn main() {
     assert_eq!(json, "\"cat\"");
     let value: Animal = serde_json::from_str(&json).unwrap();
     assert_eq!(value, Animal::Cat);
-    let json = serde_json::to_string(&Animal::from(100u8)).unwrap();
+    let json = serde_json::to_string(&Animal::from(100i16)).unwrap();
     assert_eq!(json, "\"<Unknown>\"");
     let value: Animal = serde_json::from_str(&json).unwrap();
     assert_eq!(value, Animal::UNKNOWN);
+    assert_eq!(usize::from(value), 2);
+    assert_eq!(u8::try_from(value).unwrap(), 2);
 }
--- macros/src/ffi_enum.rs
+++ macros/src/ffi_enum.rs
@@ -1,11 +1,12 @@
 use proc_macro2::{Span, TokenStream};
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
     punctuated::Punctuated,
     spanned::Spanned,
+    token::{Comma, Enum},
     Attribute, Error, Ident, ItemEnum, Meta, Path, PathArguments, PathSegment, Result, Token, Type,
-    TypePath,
+    TypePath, Variant,
 };
 
 use crate::utils::{extract_meta_from_lists, is_doc};
@@ -36,40 +37,7 @@ pub fn handle(Args(args): Args, input: ItemEnum) -> Result<TokenStream> {
     }
 
     let mut deferred_error = None;
-    let repr = extract_repr(attrs)?
-        .unwrap_or_else(|| {
-            const MAX_8: u64 = (u8::MAX as u64) + 1;
-            const MAX_16: u64 = (u16::MAX as u64) + 1;
-            const MAX_32: u64 = (u32::MAX as u64) + 1;
-            let name = if variants.iter().all(|v| v.discriminant.is_none()) {
-                #[allow(clippy::match_overlapping_arm)]
-                match variants.len() as _ {
-                    ..=MAX_8 => "u8",
-                    ..=MAX_16 => "u16",
-                    ..=MAX_32 => "u32",
-                    _ => "u64",
-                }
-            } else {
-                let span = variants
-                    .iter()
-                    .find_map(|v| v.discriminant.as_ref())
-                    .unwrap()
-                    .1
-                    .span();
-                deferred_error = Some(
-                    Error::new(
-                        span,
-                        "Cannot infer repr type for enum with manually specified discriminants. Try manually specifiying the repr type using the `repr` attribute.",
-                    )
-                    .into_compile_error()
-                );
-                "u64"
-            };
-            Type::Path(TypePath {
-                qself: None,
-                path: Ident::new(name, enum_token.span).into(),
-            })
-        });
+    let (repr, repr_size) = parse_repr(attrs, &mut deferred_error, variants, enum_token)?;
 
     let target = Type::Path(TypePath {
         qself: None,
@@ -120,6 +88,42 @@ pub fn handle(Args(args): Args, input: ItemEnum) -> Result<TokenStream> {
         &name, &target, &origin, attrs, &mut impls,
     ));
 
+    let types = [
+        ("8", Some(size_of::<u8>())),
+        ("16", Some(size_of::<u16>())),
+        ("32", Some(size_of::<u32>())),
+        ("64", Some(size_of::<u64>())),
+        ("128", Some(size_of::<u128>())),
+        ("size", None),
+    ];
+
+    let from_impls = types.iter().map(|&(name, size)| {
+        let iname = Type::Path(TypePath {
+            qself: None,
+            path: Ident::new(&(String::from("i") + name), Span::call_site()).into(),
+        });
+        let uname = Type::Path(TypePath {
+            qself: None,
+            path: Ident::new(&(String::from("u") + name), Span::call_site()).into(),
+        });
+
+        let mut impls = TokenStream::new();
+
+        if infallible(repr_size, size) {
+            from_repr(&mut impls, &target, &iname, &uname)
+        } else {
+            try_from_repr(&mut impls, &target, &repr, &iname, &uname)
+        };
+
+        if infallible(size, repr_size) {
+            repr_from(&mut impls, &target, &repr, &iname, &uname)
+        } else {
+            repr_try_from(&mut impls, &target, &repr, &iname, &uname)
+        };
+
+        impls
+    });
+
     Ok(quote! {
         #[allow(unreachable_code)]
         const _: () = {
@@ -167,19 +171,7 @@ pub fn handle(Args(args): Args, input: ItemEnum) -> Result<TokenStream> {
                 }
             }
 
-            impl ::core::convert::From<#repr> for #target {
-                #[inline]
-                fn from(repr: #repr) -> Self {
-                    Self { repr }
-                }
-            }
-
-            impl ::core::convert::From<#target> for #repr {
-                #[inline]
-                fn from(e: #target) -> Self {
-                    e.repr
-                }
-            }
+            #(#from_impls)*
 
             #impls
         };
@@ -195,6 +187,62 @@ pub fn handle(Args(args): Args, input: ItemEnum) -> Result<TokenStream> {
 
         #deferred_error
     })
+}
+
+fn parse_repr(
+    attrs: &[Attribute],
+    deferred_error: &mut Option<TokenStream>,
+    variants: &Punctuated<Variant, Comma>,
+    enum_token: &Enum,
+) -> Result<(Type, Option<usize>)> {
+    Ok(extract_repr(attrs)?.map(|t| {
+        let st=t.to_token_stream().to_string();
+        let [_,rest@..] = st.as_bytes() else {
+            panic!("not supported repr type: {}", st);
+        };
+        let size = match rest {
+            b"8" => Some(1),
+            b"16" => Some(2),
+            b"32" => Some(4),
+            b"64" => Some(8),
+            b"size" => None,
+            _ => panic!("not supported repr type: {}", st),
+        };
+        (t, size)
+    })
+        .unwrap_or_else(|| {
+            const MAX_8: u64 = (u8::MAX as u64) + 1;
+            const MAX_16: u64 = (u16::MAX as u64) + 1;
+            const MAX_32: u64 = (u32::MAX as u64) + 1;
+            let (name,size) = if variants.iter().all(|v| v.discriminant.is_none()) {
+                #[allow(clippy::match_overlapping_arm)]
+                match variants.len() as _ {
+                    ..=MAX_8 => ("u8", Some(1)),
+                    ..=MAX_16 => ("u16", Some(2)),
+                    ..=MAX_32 => ("u32", Some(4)),
+                    _ => ("u64", Some(8)),
+                }
+            } else {
+                let span = variants
+                    .iter()
+                    .find_map(|v| v.discriminant.as_ref())
+                    .unwrap()
+                    .1
+                    .span();
+                *deferred_error = Some(
+                    Error::new(
+                        span,
+                        "Cannot infer repr type for enum with manually specified discriminants. Try manually specifiying the repr type using the `repr` attribute.",
+                    )
+                    .into_compile_error()
+                );
+                ("u64", Some(8))
+            };
+            (Type::Path(TypePath {
+                qself: None,
+                path: Ident::new(name, enum_token.span).into(),
+            }), size)
+        }))
 }
 
 fn extract_repr(attrs: &[Attribute]) -> Result<Option<Type>> {
@@ -219,4 +267,95 @@ fn extract_repr(attrs: &[Attribute]) -> Result<Option<Type>> {
     }
 
     Ok(Some(result))
+}
+
+fn infallible(from_width: Option<usize>, to_width: Option<usize>) -> bool {
+    match (from_width, to_width) {
+        (Some(from), Some(to)) => from <= to,
+        (Some(from), None) => from <= 2,
+        (None, Some(to)) => to >= 8,
+        (None, None) => true,
+    }
+}
+
+fn try_from_repr(impls: &mut TokenStream, from: &Type, repr: &Type, to_i: &Type, to_u: &Type) {
+    impls.extend(
+    quote! {
+        impl ::core::convert::TryFrom<#from> for #to_i {
+            type Error = ::ffi_enum::Error;
+
+            #[inline]
+            fn try_from(e: #from) -> ::core::result::Result<Self, Self::Error> {
+                ::core::result::Result::Ok(<#to_i as ::core::convert::TryFrom<#repr>>::try_from(e.repr)?)
+            }
+        }
+
+        impl ::core::convert::TryFrom<#from> for #to_u {
+            type Error = ::ffi_enum::Error;
+
+            #[inline]
+            fn try_from(e: #from) -> ::core::result::Result<Self, Self::Error> {
+                ::core::result::Result::Ok(<#to_u as ::core::convert::TryFrom<#repr>>::try_from(e.repr)?)
+            }
+        }
+    });
+}
+
+fn from_repr(impls: &mut TokenStream, from: &Type, to_i: &Type, to_u: &Type) {
+    impls.extend(quote! {
+        impl ::core::convert::From<#from> for #to_i {
+            #[inline]
+            fn from(e: #from) -> Self {
+                e.repr as #to_i
+            }
+        }
+
+        impl ::core::convert::From<#from> for #to_u {
+            #[inline]
+            fn from(e: #from) -> Self {
+                e.repr as #to_u
+            }
+        }
+    });
+}
+
+fn repr_try_from(impls: &mut TokenStream, to: &Type, repr: &Type, from_i: &Type, from_u: &Type) {
+    impls.extend(
+    quote! {
+        impl ::core::convert::TryFrom<#from_i> for #to {
+            type Error = ::ffi_enum::Error;
+
+            #[inline]
+            fn try_from(repr: #from_i) -> ::core::result::Result<Self, Self::Error> {
+                ::core::result::Result::Ok(Self{repr: <#repr as ::core::convert::TryFrom<#from_i>>::try_from(repr)?})
+            }
+        }
+
+        impl ::core::convert::TryFrom<#from_u> for #to {
+            type Error = ::ffi_enum::Error;
+
+            #[inline]
+            fn try_from(repr: #from_u) -> ::core::result::Result<Self, Self::Error> {
+                ::core::result::Result::Ok(Self{repr: <#repr as ::core::convert::TryFrom<#from_u>>::try_from(repr)?})
+            }
+        }
+    });
+}
+
+fn repr_from(impls: &mut TokenStream, to: &Type, repr: &Type, from_i: &Type, from_u: &Type) {
+    impls.extend(quote! {
+        impl ::core::convert::From<#from_i> for #to {
+            #[inline]
+            fn from(repr: #from_i) -> Self {
+                Self{ repr: repr as #repr}
+            }
+        }
+
+        impl ::core::convert::From<#from_u> for #to {
+            #[inline]
+            fn from(repr: #from_u) -> Self {
+                Self{ repr: repr as #repr}
+            }
+        }
+    });
 }
